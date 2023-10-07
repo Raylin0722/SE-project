009b812d5b2511a72a4fe50426ea9675b5c18cb9
41047010s <107981898+41047010s@users.noreply.github.com>
2023-10-07 19:41:52 +0800

    冷卻時間更新

--- Assets/C#檔案/CharacterManage.cs
+++ Assets/C#檔案/CharacterManage.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CharacterManage : MonoBehaviour
 {
@@ -18,6 +19,18 @@ public class CharacterManage : MonoBehaviour
     }
 
     // Update is called once per frame
+    bool w1isUseable=true;
+    bool w2isUseable=true;
+    bool w3isUseable=true;
+    bool w4isUseable=true;
+    bool w5isUseable=true;
+    float w1CoolTime=0f;
+    float w2CoolTime=0f;
+    float w3CoolTime=0f;
+    float w4CoolTime=0f;
+    float w5CoolTime=0f;
+
+
     void Update()
     {
         passtime+=Time.deltaTime;
@@ -99,12 +112,59 @@ public class CharacterManage : MonoBehaviour
             Watermelon2.transform.position=new Vector3(7.09f, -0.87f, 0f);
             record++;
         }
+
+        if(!w1isUseable)
+        {
+            w1CoolTime+=Time.deltaTime;
+            if(w1CoolTime>=4.77f)
+            {
+                w1CoolTime=0f;
+                w1isUseable=true;
+            }
+        }
+        if(!w2isUseable)
+        {
+            w2CoolTime+=Time.deltaTime;
+            if(w2CoolTime>=3f)
+            {
+                w2CoolTime=0f;
+                w2isUseable=true;
+            }
+        }
+        if(!w3isUseable)
+        {
+            w3CoolTime+=Time.deltaTime;
+            if(w3CoolTime>=7f)
+            {
+                w3CoolTime=0f;
+                w3isUseable=true;
+            }
+        }
+        if(!w4isUseable)
+        {
+            w4CoolTime+=Time.deltaTime;
+            if(w4CoolTime>=5.5f)
+            {
+                w4CoolTime=0f;
+                w4isUseable=true;
+            }
+        }
+        if(!w5isUseable)
+        {
+            w5CoolTime+=Time.deltaTime;
+            if(w5CoolTime>=4f)
+            {
+                w5CoolTime=0f;
+                w5isUseable=true;
+            }
+        }
     }
 
     public void watermelon1Product()
     {
-        if(ButtonFunction.currentEnergy>=150)
+        if(ButtonFunction.currentEnergy>=150*Math.Pow(1.4, GameManage.level-1) && w1isUseable)
         {
+            w1isUseable=false;
             ButtonFunction.currentEnergy-=150;
             GameObject Watermelon1=Instantiate(MyWatermelonPrefabs[0], transform);
             Watermelon1.transform.position=new Vector3(-7.08f, -1f, 0f);
@@ -113,8 +173,9 @@ public class CharacterManage : MonoBehaviour
     }
     public void watermelon2Product()
     {
-        if(ButtonFunction.currentEnergy>=70)
+        if(ButtonFunction.currentEnergy>=70*Math.Pow(1.4, GameManage.level-1) && w2isUseable)
         {
+            w2isUseable=false;
             ButtonFunction.currentEnergy-=70;
             GameObject Watermelon2=Instantiate(MyWatermelonPrefabs[1], transform);
             Watermelon2.transform.position=new Vector3(-7.08f, -1f, 0f);
@@ -123,8 +184,9 @@ public class CharacterManage : MonoBehaviour
     }
     public void watermelon3Product()
     {
-        if(ButtonFunction.currentEnergy>=250)
+        if(ButtonFunction.currentEnergy>=250*Math.Pow(1.4, GameManage.level-1) && w3isUseable)
         {
+            w3isUseable=false;
             ButtonFunction.currentEnergy-=250;
             GameObject Watermelon3=Instantiate(MyWatermelonPrefabs[2], transform);
             Watermelon3.transform.position=new Vector3(-7.08f, -1f, 0f);
@@ -133,8 +195,9 @@ public class CharacterManage : MonoBehaviour
     }
     public void watermelon4Product()
     {
-        if(ButtonFunction.currentEnergy>=150)
+        if(ButtonFunction.currentEnergy>=150*Math.Pow(1.4, GameManage.level-1) && w4isUseable)
         {
+            w4isUseable=false;
             ButtonFunction.currentEnergy-=150;
             GameObject Watermelon4=Instantiate(MyWatermelonPrefabs[3], transform);
             Watermelon4.transform.position=new Vector3(-7.08f, -1f, 0f);
@@ -143,8 +206,9 @@ public class CharacterManage : MonoBehaviour
     }
     public void watermelon5Product()
     {
-        if(ButtonFunction.currentEnergy>=200)
+        if(ButtonFunction.currentEnergy>=200*Math.Pow(1.4, GameManage.level-1) && w5isUseable)
         {
+            w5isUseable=false;
             ButtonFunction.currentEnergy-=200;
             GameObject Watermelon5=Instantiate(MyWatermelonPrefabs[4], transform);
             Watermelon5.transform.position=new Vector3(-7.08f, -1f, 0f);
--- Assets/C#檔案/MyW4.cs
+++ Assets/C#檔案/MyW4.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class MyW4 : MonoBehaviour
 {
     // Start is called before the first frame update
+    static public float blood=(float)(600*Math.Pow(1.2, (GameManage.level-1)));
+
     void Start()
     {
         
     }
 
-    float speed=3.1f;
+    float speed=(float)(3.1*Math.Pow(1.1, (GameManage.level-1)));
     // Update is called once per frame
     void Update()
     {
