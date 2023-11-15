009ef6aa0bfe925857abd094f32624b94f36d9d6
Raylin0722 <107912521+Raylin0722@users.noreply.github.com>
2023-11-15 15:01:19 +0800

    修改抽取寶箱功能（測試中）

--- Assets/PVE/C#檔案/login/server/server.py
+++ Assets/PVE/C#檔案/login/server/server.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 config = {
     'user': 'root',        
-    'password': 'test',        
+    'password': 'gpgpr87693',        
     'database': 'SE_project',        
     'host': 'localhost',        
     'port': '3306'        
@@ -108,8 +108,10 @@ def login():
 
 @app.route("/openChest", methods=['GET', 'POST'])
 def checkLegal():#判斷是否可開啟寶箱
-    type = request.form.get('type')
-    token = request.form.get('token')
+    openType = request.args.get('openType')
+    token = request.args.get('token')
+    #openType = request.form.get('openType')
+    #token = request.form.get('token')
     
     cnx = mysql.connector.connect(**config)
     cur = cnx.cursor()
@@ -118,73 +120,91 @@ def checkLegal():#判斷是否可開啟寶箱
     cur.execute(checkquery)
 
     result = cur.fetchall()
+
+    returnResult = {"sucess" : False}
     
-    if len(result) == 1 and type: #normal
-        timequery = "SELECT money, exp, tear, props, `character` FROM usersdata WHERE token='{0}'".format(token)
+
+    if len(result) == 1 and openType: #normal
+        timequery = "SELECT money, expLevel, expTotal, tear, props, `character` , chestTime FROM usersdata WHERE token='{0}'".format(token) 
+        # money : 0 expLevel : 1 expTotal : 2 character : 3 tear : 4 chestTime : 5 props : 6
         cur.execute(timequery)
         timeresult = cur.fetchall()
-        
         if len(timeresult) == 1 :
-            timeresult[0][1] = json.loads(timeresult[0][1][1:-1])
             currentDatetime = datetime.now()
-            if timeresult[0][0] <= currentDatetime:
-                data = openChest(type)
+            print(timeresult[0][6] <= currentDatetime)
+            if timeresult[0][6] <= currentDatetime:
+                data = openChest(openType)
                 if data['result'] == 0:
-                    ()
+                    money = timeresult[0][0] + data["num"]["money"]
                 elif data['result'] == 1:
-                    ()
+                    expTotal = timeresult[0][2] + data['num']["exp"]
+                    expLevel = timeresult[0][1]
+                    if expTotal > 500 * (2.5 ** (expLevel - 2)):
+                        expTotal -= 500 * (2.5 ** (expLevel - 2))
+                        expLevel += 1
                 elif data['result'] == 2:
-                    ()
+                    tear = timeresult[0][4] + data['num']["tear"]
                 elif data['result'] == 3:
-                    ()
+                    props = json.loads(timeresult[0][6][1:-1])
+                    props[data['num']["props"]] += 1
                 elif data['result'] == 4:
-                    ()
+                    character = json.loads(timeresult[0][3][1:-1])
+                    if character[data['num']["normalCharacter"]] == 0:
+                        character[data['num']["normalCharacter"]] = 1
+                    else:
+                        money = timeresult[0][0] + 500
+                returnResult["sucess"] = True     
 
-                
-
-            
-    elif len(result) == 1 and (not type): # rare
-        tearcheck = "SELECT money, exp, tear, props, `character` FROM usersdata WHERE token='{0}'".format(token) # 要調整
+    elif len(result) == 1 and (not openType): # rare
+        tearcheck = "SELECT money, expLevel, expTotal, tear, props, `character` FROM usersdata WHERE token='{0}'".format(token) # 要調整
+        # money : 0 expLevel : 1 expTotal : 2 character : 3 tear : 4 props : 5
         cur.execute(tearcheck)
         tearresult = cur.fetchall()
         
-        if len(tearresult) == 1 and tearresult[0][0] > 10:
-            data = openChest(type)
+        print("tearresult", end=' ')
+        print(tearresult)
+        if len(tearresult) == 1 and tearresult[0][4] > 10:# 這裡要調整
+            data = openChest(openType)
             if data['result'] == 0:
-                    ()
+                    money = timeresult[0][0] + data["money"]
             elif data['result'] == 1:
-                ()
+                expTotal = timeresult[0][2] + data["exp"]
+                expLevel = timeresult[0][1]
+                if expTotal > 500 * (2.5 ** (expLevel - 2)):
+                    expTotal -= 500 * (2.5 ** (expLevel - 2))
+                    expLevel += 1
             elif data['result'] == 2:
-                ()
+                tear = timeresult[0][4] + data["tear"]
             elif data['result'] == 3:
-                ()
-            elif data['result'] == 4:
-                ()
+                props = json.loads(timeresult[0][5][1:-1])
+                props[data["props"]] += 1
+            elif data['result'] == 4 :
+                character = json.loads(timeresult[0][3][1:-1])
+                if character[data["normalCharacter"]] == 0:
+                    character[data["normalCharacter"]] = 1
+                else:
+                    money = timeresult[0][0] + 500
+            elif data['result'] == 5:
+                character = json.loads(timeresult[0][3][1:-1])
+                if character[data["normalCharacter"]] == 0:
+                    character[data["normalCharacter"]] = 1
+                else:
+                    money = timeresult[0][0] + 500
+            returnResult["sucess"] = True
 
     cur.close()
     cnx.close() 
-    return 
 
-def openChest(type:bool): # type true : normal type : false rare
+    return returnResult
+
+def openChest(openType:bool): # openType true : normal openType : false rare
     normalChoice = [50, 85, 90, 95, 100] # 錢 經驗 淚水 道具 普通
     normalItemofChest = {
         0 : {"money" : 250}, 
         1 : {"exp" : 450},
         2 : {"tear" : 2},
-        3 : {"props" : [[
-                "aaa",
-                "bbb",
-                "ccc",
-                "ddd" #代填ID
-            ],200
-        ]},
-        4 : {"normalCharacter" : [[
-                "a",
-                "b",
-                "c",
-                "d" #代填ID
-            ],200
-        ]}
+        3 : {"props" : [2]},
+        4 : {"normalCharacter" : [1, 4, 5]}
     }
 
     rareChoice = [40, 70, 80, 85, 90, 100] # 錢 經驗 淚水 道具 普通 稀有
@@ -192,47 +212,31 @@ def openChest(type:bool): # type true : normal type : false rare
         0 : {"money" : 350}, 
         1 : {"exp" : 900},
         2 : {"tear" : 5},
-        3 : {"props" : [[
-                "aaa",
-                "bbb",
-                "ccc",
-                "ddd" #代填ID
-            ],200
-        ]},
-        4 : {"normalCharacter" : [[
-                "a",
-                "b",
-                "c",
-                "d" #代填ID
-            ],200
-        ]},
-        
-        5 :  {"rareCharacter" : [[
-                "aa",
-                "bb",
-                "cc",
-                "dd" #代填ID
-            ],200
-        ]}
-        
+        3 : {"props" : [[2], 1]}, # 1 冷風 2 炸彈 
+        4 : {"normalCharacter" : [1, 4, 5]}, # 1 天使 2 小小人 3 肌肉男 4 沒穿衣服 5 小女孩 6 蝸哞 7 工程師 未決定哪個是稀有哪個是普通
+        5 : {"rareCharacter" : [2, 3, 6, 7]}
     }
 
     choice = secrets.randbelow(100)
 
-    for i in range(5 if type else 6):
-        if choice < (normalChoice[i] if type else rareChoice[i]):
+    for i in range(5 if openType else 6):
+        if choice < (normalChoice[i] if openType else rareChoice[i]):
             choice = i
-            resultofOpen = (normalItemofChest[i] if type else rareItemofChest[i])
+            resultofOpen = (normalItemofChest[i] if openType else rareItemofChest[i])
             break
 
+    print(resultofOpen)
     if choice < 3: #錢 經驗 淚水 直接回傳
         return {"result" : choice, "num" : resultofOpen}
     elif choice == 3: #道具
-        return {"result" : choice, "props" : [ secrets.choice(resultofOpen["props"][0]), 200]}
+        print(resultofOpen["props"])
+        return {"result" : choice, "props" : secrets.choice(resultofOpen["props"])}
     elif choice == 4: #角色 
-        return {"result" : choice, "normalCharacter" : [ secrets.choice(resultofOpen["normalCharacter"][0]), 200]}
-    elif (not type) and choice == 5:
-        return {"result" : choice, "rareCharacter" : [ secrets.choice(resultofOpen["rareCharacter"][0]), 200]}
+        print(resultofOpen["normalCharacter"])
+        return {"result" : choice, "normalCharacter" : secrets.choice(resultofOpen["normalCharacter"])}
+    elif (not openType) and choice == 5:
+        print(resultofOpen["rareCharacter"])
+        return {"result" : choice, "rareCharacter" :  secrets.choice(resultofOpen["rareCharacter"])}
 
 @app.route("/updateData", methods=['GET', 'POST'])
 def updateData():
