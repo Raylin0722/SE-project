7114120031a7f60dffa187e412c2e7929f5200b5
41047010s <107981898+41047010s@users.noreply.github.com>
2023-11-24 17:24:34 +0800

    儲直update

---
+++ Assets/charge/chargeControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chargeControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    [SerializeField] GameObject freesia;
+    [SerializeField] GameObject bank;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
