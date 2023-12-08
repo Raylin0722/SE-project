469051c2e9fcc8336522e1024d0a89f0e2b0dc89
0909561981 <104056231+0909561981@users.noreply.github.com>
2023-12-08 20:10:23 +0800

    更新故事背景動畫 排行榜

---
+++ Assets/排行榜/Ranking_player_information.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Ranking_player_information_Method
+{
+public class Ranking_player_information : MonoBehaviour
+{
+    public Text Rank_number; // the ranking number of the player(text)
+    public int Rank_number_index = 3; // the ranking number of the player(int)
+    public Image[] Faction; // the faction of the player
+    public Text User_name; // the name of the player
+    public Text Max_Grade; // the max progress about PVE of the player
+    public Image Myself;
+    public GameObject[] Frame;
+    private ServerMethod.Server ServerScript; // Server.cs
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ServerScript = FindObjectOfType<ServerMethod.Server>();
+        if(Myself.tag=="15")    Rank_number_index = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Update_Ranking_player_information();
+    }
+
+    // Update the player's ranking information
+    public void Update_Ranking_player_information()
+    {   
+        if(Rank_number_index>=3 && Myself.tag!="15")
+        {
+            Myself.rectTransform.anchoredPosition = new Vector3(2f,3085f-140*(Rank_number_index-3),0f);
+        }
+        if(Myself.tag=="15")
+        {
+            string MyName = ServerScript.username;
+            for(int i = 0; i<50 ; i++)
+            {
+                if(ServerScript.rankName[i]==MyName)
+                {
+                    Rank_number_index = i;
+                }
+            }
+            for(int i = 0; i<Frame.Length ;i++)
+            {
+                Frame[i].gameObject.SetActive(false);
+                if(i==Rank_number_index)
+                {
+                    Frame[i].gameObject.SetActive(true);
+                }
+                else if(Rank_number_index>3)
+                {
+                    Frame[3].gameObject.SetActive(true);
+                }
+            }
+        }
+
+        Rank_number.text = (Rank_number_index+1).ToString();
+        if(Myself.tag=="15" && (Rank_number_index+1)>=50)   Rank_number.text = "50+";
+        User_name.text = ServerScript.rankName[Rank_number_index];
+        for(int i = 0; i<5 ; i++)   Faction[i].gameObject.SetActive(false);
+        if(ServerScript.rankFaction[Rank_number_index]==-1)     Faction[4].gameObject.SetActive(true);
+        else
+        {
+            Faction[ServerScript.rankFaction[Rank_number_index]-2].gameObject.SetActive(true);
+        }
+        Max_Grade.text = ServerScript.rankClear[Rank_number_index];
+    }
+
+    public void Set_tag(int index)
+    {
+        Rank_number_index = index;
+    }
+}
+}
