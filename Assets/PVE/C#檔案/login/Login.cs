1f85c1593183f4cdd80dbff6f6522519e3f5bcf7
0909561981 <104056231+0909561981@users.noreply.github.com>
2023-12-13 16:55:28 +0800

    交換頭貼回傳給Server PVE後開寶箱問題修復 登入註冊使用者同意書

--- Assets/PVE/C#檔案/login/Login.cs
+++ Assets/PVE/C#檔案/login/Login.cs
@@ -14,6 +14,7 @@ public class Login : MonoBehaviour {
     public Image user_existed;
     public Image user_not_found;
     public Image incorrect_password;
+    public Image User_Low_Length;
     [SerializeField] string token;
 
     private void Start() {
@@ -70,53 +71,64 @@ public class Login : MonoBehaviour {
     }
 
     IEnumerator LoginPlayer() {
-        WWWForm form = new WWWForm();
-        form.AddField("username", nameField.text);
-        form.AddField("password", passwordField.text);
+        if(nameField.text.Length<4)
+        {
+            User_Low_Length.gameObject.SetActive(true);
+            submitButton.interactable = false;
+            yield return new WaitForSeconds(2f);
+            User_Low_Length.gameObject.SetActive(false);
+            submitButton.interactable = true;
+        }
+        else
+        {
+            WWWForm form = new WWWForm();
+            form.AddField("username", nameField.text);
+            form.AddField("password", passwordField.text);
 
-        UnityWebRequest www = UnityWebRequest.Post("https://pc167.csie.ntnu.edu.tw//login", form);
-        // UnityWebRequest www = UnityWebRequest.Post("http://localhost:5000/login", form);
-        
-        yield return www.SendWebRequest();
+            UnityWebRequest www = UnityWebRequest.Post("https://pc167.csie.ntnu.edu.tw//login", form);
+            // UnityWebRequest www = UnityWebRequest.Post("http://localhost:5000/login", form);
+            
+            yield return www.SendWebRequest();
 
-        if(www.downloadHandler.text[0] == '0') {
-            Debug.Log("User Login Successfully.");
-            TokenManager.Instance.Token = www.downloadHandler.text.Split('\t')[1];
-            TokenManager.Instance.Username = nameField.text;
-            UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
-        }else {
-            Debug.Log("User login failed. Error #" + www.downloadHandler.text);
-            switch(www.downloadHandler.text[0]) {
-                case '1':
-                    password_too_short.enabled = true;
-                    submitButton.interactable = false;
-                    yield return new WaitForSeconds(2f);
-                    password_too_short.enabled = false;
-                    submitButton.interactable = true;
-                    break;
-                case '2':
-                    user_existed.enabled = true;
-                    submitButton.interactable = false;
-                    yield return new WaitForSeconds(2f);
-                    user_existed.enabled = false;
-                    submitButton.interactable = true;
-                    break;
-                case '3':
-                    user_not_found.enabled = true;
-                    submitButton.interactable = false;
-                    yield return new WaitForSeconds(2f);
-                    user_not_found.enabled = false;
-                    submitButton.interactable = true;
-                    break;
-                case '4':
-                    incorrect_password.enabled = true;
-                    submitButton.interactable = false;
-                    yield return new WaitForSeconds(2f);
-                    incorrect_password.enabled = false;
-                    submitButton.interactable = true;
-                    break;
-                default:
-                    break;
+            if(www.downloadHandler.text[0] == '0') {
+                Debug.Log("User Login Successfully.");
+                TokenManager.Instance.Token = www.downloadHandler.text.Split('\t')[1];
+                TokenManager.Instance.Username = nameField.text;
+                UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+            }else {
+                Debug.Log("User login failed. Error #" + www.downloadHandler.text);
+                switch(www.downloadHandler.text[0]) {
+                    case '1':
+                        password_too_short.enabled = true;
+                        submitButton.interactable = false;
+                        yield return new WaitForSeconds(2f);
+                        password_too_short.enabled = false;
+                        submitButton.interactable = true;
+                        break;
+                    case '2':
+                        user_existed.enabled = true;
+                        submitButton.interactable = false;
+                        yield return new WaitForSeconds(2f);
+                        user_existed.enabled = false;
+                        submitButton.interactable = true;
+                        break;
+                    case '3':
+                        user_not_found.enabled = true;
+                        submitButton.interactable = false;
+                        yield return new WaitForSeconds(2f);
+                        user_not_found.enabled = false;
+                        submitButton.interactable = true;
+                        break;
+                    case '4':
+                        incorrect_password.enabled = true;
+                        submitButton.interactable = false;
+                        yield return new WaitForSeconds(2f);
+                        incorrect_password.enabled = false;
+                        submitButton.interactable = true;
+                        break;
+                    default:
+                        break;
+                }
             }
         }
     }
